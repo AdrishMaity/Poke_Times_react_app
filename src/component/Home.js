dda551f013836f504d529beae4747b86b3324b7d
Adrish <adrish.maity@outlook.com>
2018-12-07 13:16:50 +0530

    Added axios to process http requests from third party website

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 
-const Home = () => {
-    return(
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit ab soluta vero qui, voluptate modi tenetur, error expedita est pariatur repudiandae? Deserunt ut amet reiciendis recusandae eius rem autem fuga?</p>
-        </div>
-    )
+class Home extends Component {
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+            console.log(res)
+        })
+    }
+    render () {
+        return(
+            <div className="container">
+                <h4 className="center">Home</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit ab soluta vero qui, voluptate modi tenetur, error expedita est pariatur repudiandae? Deserunt ut amet reiciendis recusandae eius rem autem fuga?</p>
+            </div>
+        )
+    }
 }
 
 export default Home
