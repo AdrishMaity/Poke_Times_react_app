f67b5d44a54ffd97e01f909141e7c101b058c915
Adrish <adrish.maity@outlook.com>
2018-12-07 16:21:12 +0530

    Added seperate action files and defined the deletePost function. Then the function was called from the action file

--- src/component/Post.js
+++ src/component/Post.js
@@ -1,6 +1,6 @@
 import React, { Component }from 'react';
 import { connect } from 'react-redux';
-
+import { deletePost } from '../actions/postActions'
 
 class Post extends Component{
 
@@ -43,7 +43,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        deletePost: (id) => { dispatch({ type: 'DELETE_POST', id: id }) } 
+        // deletePost: (id) => { dispatch({ type: 'DELETE_POST', id: id }) } 
+        deletePost: (id) => { dispatch(deletePost(id)) } 
     }
 }
 
